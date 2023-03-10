64dc56ab5e529c0b696864d2f2466c306d94397a
nyius <josephdesigned@gmail.com>
2023-03-09 22:09:30 -0500

    news bar now gets patch notes and dev diaries.

--- server/index.js
+++ server/index.js
@@ -12,10 +12,34 @@ app.use(cors());
 
 // Gets the news articles
 app.get('/news', async (req, res) => {
-	const data = await performScraping();
+	const data = [];
+
+	const newsData = await scrapeNews();
+	const patchData = await scrapePatchNotes();
+	const devData = await scrapeDevDiaries();
+
+	// add all the article to one array
+	for (const article in newsData) {
+		data.push(newsData[article]);
+	}
+	for (const article in patchData) {
+		data.push(patchData[article]);
+	}
+	for (const article in devData) {
+		data.push(devData[article]);
+	}
+
+	// sort the array by date
+	data.sort((a, b) => {
+		const aDate = new Date(a.date);
+		const bDate = new Date(b.date);
+		return bDate - aDate;
+	});
+
+	// return 20 articles
 	res.json({
 		message: 'news',
-		data,
+		data: JSON.stringify(data.splice(0, 20)),
 	});
 });
 
@@ -25,12 +49,11 @@ app.listen(PORT, () => {
 });
 
 /**
- * Handles scraping the KSP website for articles
+ * Handles scraping the KSP website for news articles
  * @returns
  */
-const performScraping = async () => {
+const scrapeNews = async () => {
 	const articles = [];
-
 	// Scrape the new articles
 	const axiosResponse = await axios.request({
 		method: 'GET',
@@ -55,11 +78,90 @@ const performScraping = async () => {
 				image,
 				date,
 				title,
+				type: 'news',
 				url: 'https://www.kerbalspaceprogram.com/news',
 			};
 
 			articles.push(article);
 		});
-	const dataJSON = JSON.stringify(articles);
+	const dataJSON = articles;
+	return dataJSON;
+};
+/**
+ * Handles scraping the KSP website for patch notes
+ * @returns
+ */
+const scrapePatchNotes = async () => {
+	const articles = [];
+	// Scrape the new articles
+	const axiosResponse = await axios.request({
+		method: 'GET',
+		url: 'https://www.kerbalspaceprogram.com/patch-notes',
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+		},
+	});
+
+	const $ = cheerio.load(axiosResponse.data);
+
+	const htmlElement = $('.css-180q35u');
+
+	$('.css-180q35u')
+		.find('.css-41n5y6')
+		.each((i, element) => {
+			// Extract data
+			const image = $(element).find('.css-1hbpsw3').find('.css-llahnu').find('img').attr('srcset');
+			const date = $(element).find('.css-jwjfk5').find('.css-og1fvt').find('time').text();
+			const title = $(element).find('.css-jwjfk5').find('.css-kv05w6').text();
+			const article = {
+				image,
+				date,
+				title,
+				type: 'Patch Notes',
+				url: 'https://www.kerbalspaceprogram.com/patch-notes',
+			};
+
+			articles.push(article);
+		});
+	const dataJSON = articles;
+	return dataJSON;
+};
+/**
+ * Handles scraping the KSP website for patch notes
+ * @returns
+ */
+const scrapeDevDiaries = async () => {
+	const articles = [];
+	// Scrape the new articles
+	const axiosResponse = await axios.request({
+		method: 'GET',
+		url: 'https://www.kerbalspaceprogram.com/dev-diaries',
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+		},
+	});
+
+	const $ = cheerio.load(axiosResponse.data);
+
+	const htmlElement = $('.css-180q35u');
+
+	$('.css-180q35u')
+		.find('.css-41n5y6')
+		.each((i, element) => {
+			// Extract data
+			const image = $(element).find('.css-1hbpsw3').find('.css-llahnu').find('img').attr('srcset');
+			const date = $(element).find('.css-jwjfk5').find('.css-og1fvt').find('time').text();
+			const title = $(element).find('.css-jwjfk5').find('.css-kv05w6').text();
+			const article = {
+				image,
+				date,
+				title,
+				type: 'Dev Diary',
+				url: 'https://www.kerbalspaceprogram.com/dev-diaries',
+			};
+
+			articles.push(article);
+		});
+	const dataJSON = articles;
 	return dataJSON;
 };
--- src/components/news/NewsCard.jsx
+++ src/components/news/NewsCard.jsx
@@ -5,8 +5,11 @@ function NewsCard({ article }) {
 	const navigate = useNavigate();
 
 	return (
-		<a className="rounded-lg bg-base-900 cursor-pointer hover:bg-slate-700" href={article.url} target="_blank">
-			<img src={article.image} alt="" className="w-full" />
+		<a className="rounded-lg bg-base-900 cursor-pointer hover:bg-slate-700 relative" href={article.url} target="_blank">
+			<div className="relative">
+				<img src={article.image} alt="" className="w-full" />
+				<div className="badge text-xl 2k:text-2xl p-3 2k:p-5 absolute bottom-2 right-2">{article.type}</div>
+			</div>
 			<div className="flex flex-col p-4 2k:p-8">
 				<p className="text-lg 2k:text-2xl text-slate-500">{article.date}</p>
 				<p className="text-lg 2k:text-2xl text-slate-200">{article.title}</p>

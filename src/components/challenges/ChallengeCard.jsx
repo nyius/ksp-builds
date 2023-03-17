789bc7204cc8e2781319bc1aaf049cea98555696
nyius <josephdesigned@gmail.com>
2023-03-17 02:03:42 -0400

    button fix

--- src/components/challenges/ChallengeCard.jsx
+++ src/components/challenges/ChallengeCard.jsx
@@ -9,7 +9,7 @@ function ChallengeCard({ challenge, i, currentSlide }) {
 				<div className="flex flex-col">
 					<p className="text-3xl 2k:text-5xl text-white font-bold z-50">{challenge.title}</p>
 					<p className="text-lg 2k:text-2xl italic text-slate-500 mb-10 2k:mb-20 z-50">{challenge.date}</p>
-					<Button type="ahref" target="_blank" href={challenge.url} text="Read more" icon="right" color="btn-primary" position="z-50" />
+					<Button type="ahref" target="_blank" href={challenge.url} text="Read more" icon="right" color="btn-primary" position="z-50" size="w-fit" />
 				</div>
 			</>
 		);

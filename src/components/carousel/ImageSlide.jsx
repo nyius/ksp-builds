34f5ded50653c9675696ae219b8f9bd6197a5d9d
nyius <josephdesigned@gmail.com>
2023-03-21 20:26:33 -0400

    quick carousel fix

--- src/components/carousel/ImageSlide.jsx
+++ src/components/carousel/ImageSlide.jsx
@@ -4,14 +4,14 @@ function ImageSlide({ length, image, i }) {
 	const slideNum = (i += 1);
 	return (
 		<div id={`slide${slideNum}`} className="carousel-item relative w-full">
-			<img src={image} className="w-full object-scale-down" />
+			<img src={image} className="w-full object-contain" />
 			<div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
 				{length > 1 && (
 					<>
-						<a href={slideNum === 1 ? `#slide${length}` : `#slide${slideNum - 1}`} className="btn btn-circle 2k:btn-lg 2k:text-4xl bg-base-600 z-100 drop-shadow-lg">
+						<a href={slideNum === 1 ? `#slide${length}` : `#slide${slideNum - 1}`} className="btn btn-circle 2k:btn-lg 2k:text-4xl bg-base-600 z-100 drop-shadow-lg z-50">
 							❮
 						</a>
-						<a href={slideNum === length ? `#slide1` : `#slide${slideNum + 1}`} className="btn btn-circle 2k:btn-lg 2k:text-4xl bg-base-600 z-100 drop-shadow-lg">
+						<a href={slideNum === length ? `#slide1` : `#slide${slideNum + 1}`} className="btn btn-circle 2k:btn-lg 2k:text-4xl bg-base-600 z-100 drop-shadow-lg z-50">
 							❯
 						</a>
 					</>

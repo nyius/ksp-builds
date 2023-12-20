a99cdd478b2303b3ac8f7cb8b3cbd096ee19f618
nyius <josephdesigned@gmail.com>
2023-12-20 10:54:08 -0500

    quick change to the news card default image

--- src/components/cards/NewsCard.jsx
+++ src/components/cards/NewsCard.jsx
@@ -11,7 +11,7 @@ function NewsCard({ article }) {
 		<>
 			<a className="flex flex-col rounded-lg bg-base-200 cursor-pointer relative min-w-12 max-w-4x" href={article.url} target="_blank">
 				<div className="hover:bg-primary hover:shadow-xl h-full flex flex-col place-content-between">
-					<img src={article.image ? article.image : DefaultArticleImage} alt="" className="w-full " />
+					<img src={article.image ? article.image : 'https://firebasestorage.googleapis.com/v0/b/kspbuilds.appspot.com/o/kspbuilds_update_default.png?alt=media&token=daa83171-d7f5-4292-b89c-b15ff06b31c6'} alt="" className="w-full " />
 					<div className="flex flex-col p-4 2k:p-8">
 						<p className="text-lg 2k:text-2xl text-slate-400">{article.date}</p>
 						<p className="text-lg 2k:text-2xl text-slate-100">{article.title}</p>
